2c7df0e26a58d4ce67a9a0ad36aa71c512d5f39d
Joe Sweeney <joesweeney@Joes-MacBook-Air.local>
2025-07-26 17:14:19 -0400

    New: Dashboard

---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,20 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      hide = {
+        statusline = false,
+        tabline = false,
+        winbar = false,
+      },
+      shortcut_type = 'number',
+      config = {
+        packages = { enabled = false },
+        project = { enabled = true, limit = 3 },
+        mru = { enabled = true, limit = 5 },
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -49,4 +49,6 @@ return {
       }
     end,
   },
+
+  { import = 'custom.plugins.dashboard' },
 }
