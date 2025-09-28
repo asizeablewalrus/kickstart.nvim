5a6c1e69a708b24de1f055d2f99290549ce2d26d
asizeablewalrus <jcs0810@outlook.com>
2025-09-28 14:37:47 -0500

    altered lualine and cleaned up garbage

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,27 +1,28 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    styles = {
-      comments = { italics = true },
-    },
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    flavour = 'auto',
-    auto_integrations = true,
-    show_end_of_buffer = true,
-  },
-  {
-    'navarasu/onedark.nvim',
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('onedark').setup {
-        style = 'dark',
-        ending_tildes = true,
-      }
-    end,
-  },
+
+	{
+		'folke/tokyonight.nvim',
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		styles = {
+			comments = { italics = true },
+		},
+	},
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 1000,
+		flavour = 'auto',
+		auto_integrations = true,
+		show_end_of_buffer = true,
+	},
+	{
+		'navarasu/onedark.nvim',
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require('onedark').setup {
+				style = 'dark',
+				ending_tildes = true,
+			}
+		end,
+	},
 }
